80799b4af2fef104bcf95adcf4c290a62827b850
toshiykst <txsxx104@gmail.com>
2021-01-12 00:36:37 +0900

    Add gorm

--- app/server.go
+++ app/server.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func userHandler(c echo.Context) error {
@@ -12,7 +15,36 @@ func userHandler(c echo.Context) error {
 	return c.String(http.StatusOK, id)
 }
 
+// Post is a structure of the post.
+type Post struct {
+	gorm.Model
+	Title   string
+	Content string
+}
+
 func main() {
+	connection := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true&loc=Local",
+		"user",
+		"password",
+		"db",
+		"3306",
+		"golang_rest_api",
+	)
+	db, err := gorm.Open(mysql.Open(connection), &gorm.Config{})
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	db.Set("gorm:table_options", "ENGINE=InnoDB")
+
+	// Migrate the schema
+	db.AutoMigrate(&Post{})
+
+	// Test Create
+	db.Create(&Post{Title: "title", Content: "content"})
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/toshiykst/golang-rest-api
 
 go 1.15
 
-require github.com/labstack/echo/v4 v4.1.17
+require (
+	github.com/labstack/echo/v4 v4.1.17
+	gorm.io/driver/mysql v1.0.3
+	gorm.io/gorm v1.20.11
+)
