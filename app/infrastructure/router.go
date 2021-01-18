eae1c038a5ba9faa808ef7d3e50856a5ea21f8fd
toshiykst <txsxx104@gmail.com>
2021-01-19 01:05:53 +0900

    Add Create post

--- app/infrastructure/router.go
+++ app/infrastructure/router.go
@@ -1,23 +1,23 @@
 package infrastructure
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+
+	"github.com/toshiykst/golang-rest-api/app/interfaces/controllers"
 )
 
 // RunRouter runs router.
 func RunRouter() {
 	e := echo.New()
-	// d := NewDB()
+	db := NewDB()
+
+	postController := controllers.NewPostController(db)
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	e.POST("/posts", func(c echo.Context) error { return postController.Create(c) })
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
--- app/interfaces/controllers/post_controller.go
+++ app/interfaces/controllers/post_controller.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"fmt"
+
 	"github.com/toshiykst/golang-rest-api/app/interfaces/db"
 )
 
@@ -11,4 +13,10 @@ type PostController struct {
 // NewPostController creates an instance of PostController.
 func NewPostController(db db.DB) *PostController {
 	return &PostController{}
+}
+
+// Create creates post
+func (controller *PostController) Create(c Context) (err error) {
+	fmt.Println("Not impremented Create...")
+	return
 }
