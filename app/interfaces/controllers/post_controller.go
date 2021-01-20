a422410af24ecd3d0b1a014a8cfe4735cda5d9cf
toshiykst <yxkxtxsx1075@gmail.com>
2021-01-21 00:32:50 +0900

    Implement NewPostController

--- app/interfaces/controllers/post_controller.go
+++ app/interfaces/controllers/post_controller.go
@@ -4,15 +4,23 @@ import (
 	"fmt"
 
 	"github.com/toshiykst/golang-rest-api/app/interfaces/db"
+	"github.com/toshiykst/golang-rest-api/app/usecase/interactor"
 )
 
 // PostController is a structure of PostController.
 type PostController struct {
+	Interactor interactor.PostInteractor
 }
 
 // NewPostController creates an instance of PostController.
-func NewPostController(db db.DB) *PostController {
-	return &PostController{}
+func NewPostController(d db.DB) *PostController {
+	return &PostController{
+		Interactor: interactor.PostInteractor{
+			PostRepository: &db.PostRepository{
+				DB: d,
+			},
+		},
+	}
 }
 
 // Create creates post
