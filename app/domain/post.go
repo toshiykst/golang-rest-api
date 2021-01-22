34ac4364372f6d9a7e1c9b5fb15e3840aa74c64a
toshiykst <yxkxtxsx1075@gmail.com>
2021-01-23 00:43:09 +0900

    Fix domain of post

--- app/domain/post.go
+++ app/domain/post.go
@@ -4,11 +4,11 @@ import "time"
 
 // Post is a structure of the post.
 type Post struct {
-	ID        int       `gorm:"primary_key" json:"id"`
-	Title     string    `json:"title"`
-	Content   string    `json:"content"`
-	CreatedAt time.Time `json:"-"`
-	UpdatedAt time.Time `json:"-"`
+	ID        int        `json:"id" gorm:"primary_key" `
+	Title     string     `json:"title"`
+	Content   string     `json:"content"`
+	CreatedAt *time.Time `json:"-"`
+	UpdatedAt *time.Time `json:"-"`
 }
 
 // Posts is a set of posts.
